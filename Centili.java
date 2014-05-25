4b6dc70927f328032fd6e3f4fd52d3d5178ce66f
squarrel <milosb@gmx.com>
2014-05-26 00:05:47 +0200

    Update Centili.java

--- Centili.java
+++ Centili.java
@@ -2,7 +2,8 @@
 
 package org.myapp;
 
-import android.support.v7.app.ActionBarActivity;
+//import android.support.v7.app.ActionBarActivity;
+import org.renpy.android.PythonActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,58 +14,62 @@ import android.widget.Toast;
 import c.mpayments.android.*;
 
 
-public class Centili extends Activity {
+public class Centili {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    //@Override
+    //protected void onCreate(Bundle savedInstanceState) {
+        //super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
 
-        ((Button)findViewById(R.id.buttonPurchase)).setOnClickListener(mPurchaseClick);
+        //((Button)findViewById(R.id.buttonPurchase)).setOnClickListener(mPurchaseClick);
+    //}
+
+    //View.OnClickListener mPurchaseClick = new View.OnClickListener() {
+        //@Override
+        //public void onClick(View v) {
+
+    static class PurchListener implements PurchaseListener { 
+        @Override
+        public void onPurchaseSuccess(PurchaseResponse paramPurchaseResponse) {
+            // handle purchase success
+            Toast.makeText(getApplicationContext(), "Purchase success!", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onPurchasePending(PurchaseResponse paramPurchaseResponse) {
+            // notification that purchase verification has begun
+            Toast.makeText(getApplicationContext(), "Purchase pending", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onPurchaseFailed(PurchaseResponse paramPurchaseResponse) {
+            // purchase canceled by user
+            Toast.makeText(getApplicationContext(), "Purchase failed!", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onPurchaseCanceled(PurchaseResponse paramPurchaseResponse) {
+            // purchase cancelled by user
+            Toast.makeText(getApplicationContext(), "Purchase canceled", Toast.LENGTH_SHORT).show();
+        }
     }
 
-    View.OnClickListener mPurchaseClick = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-
-            PurchaseManager.attachPurchaseListener(new PurchaseListener() {
-                @Override
-                public void onPurchaseSuccess(PurchaseResponse paramPurchaseResponse) {
-                    // handle purchase success
-                    Toast.makeText(getApplicationContext(), "Purchase success!", Toast.LENGTH_SHORT).show();
-                }
-
-                @Override
-                public void onPurchasePending(PurchaseResponse paramPurchaseResponse) {
-                    // notification that purchase verification has begun
-                    Toast.makeText(getApplicationContext(), "Purchase pending", Toast.LENGTH_SHORT).show();
-                }
-
-                @Override
-                public void onPurchaseFailed(PurchaseResponse paramPurchaseResponse) {
-                    // purchase canceled by user
-                    Toast.makeText(getApplicationContext(), "Purchase failed!", Toast.LENGTH_SHORT).show();
-                }
-
-                @Override
-                public void onPurchaseCanceled(PurchaseResponse paramPurchaseResponse) {
-                    // purchase cancelled by user
-                    Toast.makeText(getApplicationContext(), "Purchase canceled", Toast.LENGTH_SHORT).show();
-                }
-            });
-
-            PurchaseRequest pr = new PurchaseRequest("API-key-here");
-            //pr.setClientId(<YOUR-CLIENT-ID>); // optional
-            //pr.setInfo("Info text..."); // optional
-            pr.setLanguageCode("EN"); //optional
-            pr.setOfflineModeEnabled(true); // optional
-            PurchaseManager.startPurchase(pr, Centili.this);
-        }
-    };
+    static PurchListener purchListener = new PurchListener();
+    
+    PurchaseManager.attachPurchaseListener(purchListener);
+    
+    PurchaseRequest pr = new PurchaseRequest("421d33a2d56fe51207add3c07290f29a");
+    //pr.setClientId(<YOUR-CLIENT-ID>); // optional
+    //pr.setInfo("Info text..."); // optional
+    pr.setLanguageCode("EN"); //optional
+    pr.setOfflineModeEnabled(true); // optional
+    PurchaseManager.startPurchase(pr, Centili.this);
+        //}
+    //};
     
     
     public void onCheckServiceAvailabilityButtonClick(View v) {
-        PurchaseManager.checkServiceAvailabilityAsync("API-key-here", Centili.this,
+        PurchaseManager.checkServiceAvailabilityAsync("421d33a2d56fe51207add3c07290f29a", Centili.this,
                 new ServiceAvailabilityListener() {
                     @Override
                     public void onServiceStatusObtained(final int status) {
@@ -83,3 +88,4 @@ public class Centili extends Activity {
     }
     
 }
+
